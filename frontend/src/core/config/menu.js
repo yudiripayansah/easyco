753db7b0fc6caaacff6f90c85c13779c39cf104e
Yudi Ripayansah <yudiripayansah@gmail.com>
2022-09-03 20:41:17 +0700

    form pengguna

--- frontend/src/core/config/menu.js
+++ frontend/src/core/config/menu.js
@@ -5,217 +5,217 @@ const menu = [
     icon: 'menu-icon flaticon2-architecture-and-city',
   },
   // Keanggotaan
-  {
-    target: '/keanggotaan',
-    label: 'Keanggotaan',
-    icon: 'menu-icon far fa-user',
-    children: [
-      {
-        target: '/keanggotaan/registrasi-anggota',
-        label: 'Registrasi Anggota'
-      },
-      {
-        target: '/keanggotaan/approval-registrasi',
-        label: 'Approval Registrasi'
-      },
-      {
-        target: '/keanggotaan/registrasi-anggota-keluar',
-        label: 'Registrasi Anggota Keluar'
-      },
-      {
-        target: '/keanggotaan/approval-registrasi-keluar',
-        label: 'Approval Registrasi Anggota Keluar'
-      },
-      {
-        target: '/keanggotaan/transaksi-anggota',
-        label: 'Transaksi Anggota'
-      },
-      {
-        target: '/keanggotaan/laporan',
-        label: 'Laporan',
-        children: [
-          {
-            target: '/keanggotaan/laporan/registrasi-anggota',
-            label: 'Registrasi Anggota'
-          },
-          {
-            target: '/keanggotaan/laporan/profil-anggota',
-            label: 'Profil Anggota'
-          },
-          {
-            target: '/keanggotaan/laporan/saldo-anggota',
-            label: 'Saldo Anggota'
-          },
-          {
-            target: '/keanggotaan/laporan/anggota-keluar',
-            label: 'Anggota Keluar'
-          },
-        ],
-      },
-    ]
-  },
+  // {
+  //   target: '/keanggotaan',
+  //   label: 'Keanggotaan',
+  //   icon: 'menu-icon far fa-user',
+  //   children: [
+  //     {
+  //       target: '/keanggotaan/registrasi-anggota',
+  //       label: 'Registrasi Anggota'
+  //     },
+  //     {
+  //       target: '/keanggotaan/approval-registrasi',
+  //       label: 'Approval Registrasi'
+  //     },
+  //     {
+  //       target: '/keanggotaan/registrasi-anggota-keluar',
+  //       label: 'Registrasi Anggota Keluar'
+  //     },
+  //     {
+  //       target: '/keanggotaan/approval-registrasi-keluar',
+  //       label: 'Approval Registrasi Anggota Keluar'
+  //     },
+  //     {
+  //       target: '/keanggotaan/transaksi-anggota',
+  //       label: 'Transaksi Anggota'
+  //     },
+  //     {
+  //       target: '/keanggotaan/laporan',
+  //       label: 'Laporan',
+  //       children: [
+  //         {
+  //           target: '/keanggotaan/laporan/registrasi-anggota',
+  //           label: 'Registrasi Anggota'
+  //         },
+  //         {
+  //           target: '/keanggotaan/laporan/profil-anggota',
+  //           label: 'Profil Anggota'
+  //         },
+  //         {
+  //           target: '/keanggotaan/laporan/saldo-anggota',
+  //           label: 'Saldo Anggota'
+  //         },
+  //         {
+  //           target: '/keanggotaan/laporan/anggota-keluar',
+  //           label: 'Anggota Keluar'
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // },
   // Tabungan
-  {
-    target: '/tabungan',
-    label: 'Tabungan',
-    icon: 'menu-icon fas fa-money-check-alt',
-    children: [
-      {
-        target: '/tabungan/pembukaan-rekening-tabungan',
-        label: 'Pembukaan Rekening Tabungan'
-      },
-      {
-        target: '/tabungan/transaksi-tabungan',
-        label: 'Transaksi Tabungan'
-      },
-      {
-        target: '/tabungan/penutupan-rekening-tabungan',
-        label: 'Penutupan Rekening Tabungan'
-      },
-      {
-        target: '/tabungan/laporan',
-        label: 'Laporan',
-        children: [
-          {
-            target: '/tabungan/laporan/pembukaan-rekening-tabungan',
-            label: 'Pembukaan Rekening Tabungan'
-          },
-          {
-            target: '/tabungan/laporan/transaksi-tabungan',
-            label: 'Transaksi Tabungan'
-          },
-          {
-            target: '/tabungan/laporan/saldo-tabungan',
-            label: 'Saldo Tabungan'
-          },
-          {
-            target: '/tabungan/laporan/statement-tabungan',
-            label: 'Statement Tabungan'
-          },
-          {
-            target: '/tabungan/laporan/penutupan-rekening-tabungan',
-            label: 'Penutupan Rekening Tabungan'
-          },
-        ],
-      },
-    ]
-  },
+  // {
+  //   target: '/tabungan',
+  //   label: 'Tabungan',
+  //   icon: 'menu-icon fas fa-money-check-alt',
+  //   children: [
+  //     {
+  //       target: '/tabungan/pembukaan-rekening-tabungan',
+  //       label: 'Pembukaan Rekening Tabungan'
+  //     },
+  //     {
+  //       target: '/tabungan/transaksi-tabungan',
+  //       label: 'Transaksi Tabungan'
+  //     },
+  //     {
+  //       target: '/tabungan/penutupan-rekening-tabungan',
+  //       label: 'Penutupan Rekening Tabungan'
+  //     },
+  //     {
+  //       target: '/tabungan/laporan',
+  //       label: 'Laporan',
+  //       children: [
+  //         {
+  //           target: '/tabungan/laporan/pembukaan-rekening-tabungan',
+  //           label: 'Pembukaan Rekening Tabungan'
+  //         },
+  //         {
+  //           target: '/tabungan/laporan/transaksi-tabungan',
+  //           label: 'Transaksi Tabungan'
+  //         },
+  //         {
+  //           target: '/tabungan/laporan/saldo-tabungan',
+  //           label: 'Saldo Tabungan'
+  //         },
+  //         {
+  //           target: '/tabungan/laporan/statement-tabungan',
+  //           label: 'Statement Tabungan'
+  //         },
+  //         {
+  //           target: '/tabungan/laporan/penutupan-rekening-tabungan',
+  //           label: 'Penutupan Rekening Tabungan'
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // },
   // Pembiayaan
-  {
-    target: '/pembiayaan',
-    label: 'Pembiayaan',
-    icon: 'menu-icon fas fa-wallet',
-    children: [
-      {
-        target: '/pembiayaan/pengajuan-pembiayaan',
-        label: 'Pengajuan Pembiayaan'
-      },
-      {
-        target: '/pembiayaan/registrasi-akad',
-        label: 'Registrasi Akad'
-      },
-      {
-        target: '/pembiayaan/pencairan',
-        label: 'Pencairan'
-      },
-      {
-        target: '/pembiayaan/transaksi-pembiayaan',
-        label: 'Transaksi Pembiayaan'
-      },
-      {
-        target: '/pembiayaan/angsuran-pembiayaan',
-        label: 'Angsuran Pembiayaan'
-      },
-      {
-        target: '/pembiayaan/pelunasan-pembiayaan',
-        label: 'Pelunasan Pembiayaan'
-      },
-      {
-        target: '/pembiayaan/laporan',
-        label: 'Laporan',
-        children: [
-          {
-            target: '/pembiayaan/laporan/pengajuan-pembiayaan',
-            label: 'Pengajuan Pembiayaan'
-          },
-          {
-            target: '/pembiayaan/laporan/pencairan',
-            label: 'Pencairan'
-          },
-          {
-            target: '/pembiayaan/laporan/outstanding',
-            label: 'Outstanding'
-          },
-          {
-            target: '/pembiayaan/laporan/penerimaan-angsuran',
-            label: 'Penerimaaan Angsuran'
-          },
-          {
-            target: '/pembiayaan/laporan/kartu-angsuran',
-            label: 'Kartu Angsuran'
-          },
-          {
-            target: '/pembiayaan/laporan/pelunasan',
-            label: 'Pelunasan'
-          },
-          {
-            target: '/pembiayaan/laporan/kolektibilitas',
-            label: 'Kolektibilitas'
-          },
-        ],
-      },
-    ]
-  },
+  // {
+  //   target: '/pembiayaan',
+  //   label: 'Pembiayaan',
+  //   icon: 'menu-icon fas fa-wallet',
+  //   children: [
+  //     {
+  //       target: '/pembiayaan/pengajuan-pembiayaan',
+  //       label: 'Pengajuan Pembiayaan'
+  //     },
+  //     {
+  //       target: '/pembiayaan/registrasi-akad',
+  //       label: 'Registrasi Akad'
+  //     },
+  //     {
+  //       target: '/pembiayaan/pencairan',
+  //       label: 'Pencairan'
+  //     },
+  //     {
+  //       target: '/pembiayaan/transaksi-pembiayaan',
+  //       label: 'Transaksi Pembiayaan'
+  //     },
+  //     {
+  //       target: '/pembiayaan/angsuran-pembiayaan',
+  //       label: 'Angsuran Pembiayaan'
+  //     },
+  //     {
+  //       target: '/pembiayaan/pelunasan-pembiayaan',
+  //       label: 'Pelunasan Pembiayaan'
+  //     },
+  //     {
+  //       target: '/pembiayaan/laporan',
+  //       label: 'Laporan',
+  //       children: [
+  //         {
+  //           target: '/pembiayaan/laporan/pengajuan-pembiayaan',
+  //           label: 'Pengajuan Pembiayaan'
+  //         },
+  //         {
+  //           target: '/pembiayaan/laporan/pencairan',
+  //           label: 'Pencairan'
+  //         },
+  //         {
+  //           target: '/pembiayaan/laporan/outstanding',
+  //           label: 'Outstanding'
+  //         },
+  //         {
+  //           target: '/pembiayaan/laporan/penerimaan-angsuran',
+  //           label: 'Penerimaaan Angsuran'
+  //         },
+  //         {
+  //           target: '/pembiayaan/laporan/kartu-angsuran',
+  //           label: 'Kartu Angsuran'
+  //         },
+  //         {
+  //           target: '/pembiayaan/laporan/pelunasan',
+  //           label: 'Pelunasan'
+  //         },
+  //         {
+  //           target: '/pembiayaan/laporan/kolektibilitas',
+  //           label: 'Kolektibilitas'
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // },
   // Keuangan
-  {
-    target: '/keuangan',
-    label: 'Keuangan',
-    icon: 'menu-icon fas fa-coins',
-    children: [
-      {
-        target: '/keuangan/pengaturan-ledger',
-        label: 'Pengaturan Ledger'
-      },
-      {
-        target: '/keuangan/pengaturan-laporan-keuangan',
-        label: 'Pengaturan Laporan Keuangan'
-      },
-      {
-        target: '/keuangan/jurnal-transaksi',
-        label: 'Jurnal Transaksi'
-      },
-      {
-        target: '/keuangan/laporan',
-        label: 'Laporan',
-        children: [
-          {
-            target: '/keuangan/laporan/jurnal',
-            label: 'Jurnal'
-          },
-          {
-            target: '/keuangan/laporan/arus-kas',
-            label: 'Arus Kas'
-          },
-          {
-            target: '/keuangan/laporan/buku-besar',
-            label: 'Buku Besar'
-          },
-          {
-            target: '/keuangan/laporan/neraca-saldo',
-            label: 'Neraca Saldo'
-          },
-          {
-            target: '/keuangan/laporan/neraca',
-            label: 'Neraca'
-          },
-          {
-            target: '/keuangan/laporan/laba-rugi',
-            label: 'Laba Rugi'
-          },
-        ],
-      },
-    ]
-  },
+  // {
+  //   target: '/keuangan',
+  //   label: 'Keuangan',
+  //   icon: 'menu-icon fas fa-coins',
+  //   children: [
+  //     {
+  //       target: '/keuangan/pengaturan-ledger',
+  //       label: 'Pengaturan Ledger'
+  //     },
+  //     {
+  //       target: '/keuangan/pengaturan-laporan-keuangan',
+  //       label: 'Pengaturan Laporan Keuangan'
+  //     },
+  //     {
+  //       target: '/keuangan/jurnal-transaksi',
+  //       label: 'Jurnal Transaksi'
+  //     },
+  //     {
+  //       target: '/keuangan/laporan',
+  //       label: 'Laporan',
+  //       children: [
+  //         {
+  //           target: '/keuangan/laporan/jurnal',
+  //           label: 'Jurnal'
+  //         },
+  //         {
+  //           target: '/keuangan/laporan/arus-kas',
+  //           label: 'Arus Kas'
+  //         },
+  //         {
+  //           target: '/keuangan/laporan/buku-besar',
+  //           label: 'Buku Besar'
+  //         },
+  //         {
+  //           target: '/keuangan/laporan/neraca-saldo',
+  //           label: 'Neraca Saldo'
+  //         },
+  //         {
+  //           target: '/keuangan/laporan/neraca',
+  //           label: 'Neraca'
+  //         },
+  //         {
+  //           target: '/keuangan/laporan/laba-rugi',
+  //           label: 'Laba Rugi'
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // },
   // Pengaturan
   {
     target: '/pengaturan',
@@ -235,29 +235,63 @@ const menu = [
         label: 'Menu'
       },
       {
-        target: '/pengaturan/lembaga',
-        label: 'Lembaga'
-      },
-      {
-        target: '/pengaturan/cabang',
-        label: 'Cabang'
-      },
-      {
-        target: '/pengaturan/wilayah',
-        label: 'Wilayah'
-      },
-      {
-        target: '/pengaturan/karyawan-petugas',
-        label: 'Karyawan / Petugas'
-      },
-      {
-        target: '/pengaturan/parameter',
-        label: 'Parameter'
-      },
-      {
-        target: '/pengaturan/tutup-periode',
-        label: 'Tutup Periode'
-      },
+        target: '/pengaturan/master-data',
+        label: 'Master Data',
+        children: [
+          {
+            target: '/pengaturan/master-data/gl',
+            label: 'Gl'
+          },
+          {
+            target: '/pengaturan/master-data/kategori-par',
+            label: 'Kategori PAR'
+          },
+          {
+            target: '/pengaturan/master-data/list-kode',
+            label: 'List Kode'
+          },
+          {
+            target: '/pengaturan/master-data/produk-pembiayaan',
+            label: 'Produk Pembiayaan'
+          },
+          {
+            target: '/pengaturan/master-data/produk-tabungan',
+            label: 'Produk Tabungan'
+          },
+          {
+            target: '/pengaturan/master-data/produk-deposito',
+            label: 'Produk Deposito'
+          },
+          {
+            target: '/pengaturan/master-data/pegawai',
+            label: 'Pegawai'
+          },
+          {
+            target: '/pengaturan/master-data/rembug',
+            label: 'Rembug'
+          },
+          {
+            target: '/pengaturan/master-data/lembaga',
+            label: 'Lembaga'
+          },
+          {
+            target: '/pengaturan/master-data/cabang',
+            label: 'Cabang'
+          },
+          {
+            target: '/pengaturan/master-data/desa',
+            label: 'Desa'
+          },
+          {
+            target: '/pengaturan/master-data/kecamatan',
+            label: 'Kecamatan'
+          },
+          {
+            target: '/pengaturan/master-data/kota-kabupaten',
+            label: 'Kota Kabupaten'
+          },
+        ]
+      }
     ]
   },
 ]
---
+++ frontend/src/pages/Pengaturan/Pengguna.vue
@@ -0,0 +1,327 @@
+<template>
+<div>
+  <h1 class="mb-5">{{$route.name}}</h1>
+  <b-card>
+    <b-row no-gutters>
+      <b-col cols="12" class="d-flex justify-content-end mb-5 pb-5 border-bottom">
+        <b-button variant="success" @click="$bvModal.show('modal-form')" v-b-tooltip.hover title="Tambah Data Baru">
+          <b-icon icon="plus"/>
+          Tambah Baru
+        </b-button>
+      </b-col>
+      <b-col cols="12" class="mb-5">
+        <b-row no-gutters>
+          <b-col cols="6">
+            <div class="w-100 max-200">
+              <b-input-group size="sm" prepend="Per Halaman">
+                <b-form-select v-model="paging.perPage" :options="opt.perPage"/>
+              </b-input-group>
+            </div>
+          </b-col>
+          <b-col cols="6" class="d-flex justify-content-end">
+            <div class="w-100 max-300">
+              <b-input-group size="sm">
+                <b-form-input/>
+                <b-input-group-append>
+                  <b-button size="sm" text="Button" variant="primary">
+                    <b-icon icon="search"/>
+                    Cari
+                  </b-button>
+                </b-input-group-append>
+              </b-input-group>
+            </div>
+          </b-col>
+        </b-row>
+      </b-col>
+      <b-col cols="12">
+        <b-table bordered outlined small striped hover :fields="table.fields" :items="table.items" show-empty :emptyText="table.loading ? 'Memuat data...' : 'Tidak ada data'">
+          <template #cell(no)="item">
+            {{item.index + 1}}
+          </template>
+          <template #cell(action)="item">
+            <b-button variant="danger" size="xs" class="mx-1" @click="doDelete(item,true)" v-b-tooltip.hover title="Hapus">
+              <b-icon icon="trash"/>
+            </b-button>
+            <b-button variant="success" size="xs" class="mx-1" @click="doUpdate(item)" v-b-tooltip.hover title="Ubah">
+              <b-icon icon="pencil"/>
+            </b-button>
+          </template>
+        </b-table>
+      </b-col>
+      <b-col cols="12" class="justify-content-end d-flex">
+        <b-pagination
+          v-model="paging.currentPage"
+          :total-rows="table.totalRows"
+          :per-page="paging.perPage"
+        ></b-pagination>
+      </b-col>
+    </b-row>
+  </b-card>
+  <b-modal title="Form Pengguna" id="modal-form" hide-footer size="lg" centered>
+    <b-form @submit="doSave()">
+      <b-row>
+        <b-col cols="6">
+          <b-form-group label="Kode User" label-for="kode">
+            <b-form-input 
+              id="kode" 
+              v-model="form.data.kode"
+              disabled/>
+          </b-form-group>
+        </b-col>
+        <b-col cols="6">
+          <b-form-group label="Nama" label-for="nama">
+            <b-form-input 
+              id="nama" 
+              v-model="$v.form.data.nama.$model"
+              :state="validateState('nama')"/>
+          </b-form-group>
+        </b-col>
+        <b-col cols="6">
+          <b-form-group label="Email" label-for="email">
+            <b-form-input 
+              id="email" 
+              v-model="$v.form.data.email.$model"
+              :state="validateState('email')"/>
+          </b-form-group>
+        </b-col>
+        <b-col cols="6">
+          <b-form-group label="Password" label-for="password">
+            <b-form-input 
+              id="password" 
+              v-model="$v.form.data.password.$model" 
+              type="password"
+              :state="validateState('password')"/>
+          </b-form-group>
+        </b-col>
+        <b-col cols="4">
+          <b-form-group label="Role" label-for="role">
+            <b-form-select 
+              id="role" 
+              v-model="$v.form.data.role.$model" 
+              :options="opt.role"
+              :state="validateState('role')"/>
+          </b-form-group>
+        </b-col>
+        <b-col cols="4">
+          <b-form-group label="Status" label-for="status">
+            <b-form-select 
+              id="status" 
+              v-model="$v.form.data.status.$model" 
+              :options="opt.status"
+              :state="validateState('status')"/>
+          </b-form-group>
+        </b-col>
+        <b-col cols="4">
+          <b-form-group label="Cabang" label-for="cabang">
+            <b-form-select 
+              id="cabang" 
+              v-model="$v.form.data.cabang.$model" 
+              :options="opt.cabang"
+              :state="validateState('cabang')"/>
+          </b-form-group>
+        </b-col>
+        <b-col cols="12" class="d-flex justify-content-end border-top pt-5">
+          <b-button variant="secondary" @click="$bvModal.hide('modal-form')" :disabled="form.loading">Cancel</b-button>
+          <b-button variant="primary" type="submit" :disabled="form.loading" class="ml-3">
+            {{form.loading ? 'Memproses...' : 'Simpan' }}
+          </b-button>
+        </b-col>
+      </b-row>
+    </b-form>
+  </b-modal>
+  <b-modal title="Delete" id="modal-delete" hide-footer size="sm" header-bg-variant="warning" body-bg-variant="warning" centered>
+    <p class="text-center py-3">Anda yakin ingin menghapus data ini?</p>
+    <div class="d-flex justify-content-end">
+      <b-button variant="light" type="button" :disabled="remove.loading">Tidak</b-button>
+      <b-button variant="danger" class="ml-3" type="button" :disabled="remove.loading" @click="doDelete(remove.data,false)">
+        {{remove.loading ? 'Memproses...' : 'Ya' }}
+      </b-button>
+    </div>
+  </b-modal>
+</div>
+</template>
+
+<script>
+import { validationMixin } from "vuelidate";
+import { required, sameAs, email, minLength } from 'vuelidate/lib/validators'
+export default {
+  name: "Pengguna",
+  components: {},
+  data() {
+    return {
+      form: {
+        data: {
+          id: null,
+          kode: 'Auto Generated',
+          nama: null,
+          email: null,
+          password: null,
+          role: null,
+          status: null,
+          cabang: null,
+        },
+        loading: false,
+      },
+      table: {
+        fields: [
+          {
+            key: 'no',
+            sortable: false,
+            label: 'No',
+            thClass: 'text-center w-5p',
+            tdClass: 'text-center'
+          },
+          {
+            key: 'kode',
+            sortable: true,
+            label: 'Kode User',
+            thClass: 'text-center',
+            tdClass: ''
+          },
+          {
+            key: 'nama',
+            sortable: true,
+            label: 'Nama',
+            thClass: 'text-center',
+            tdClass: ''
+          },
+          {
+            key: 'email',
+            sortable: true,
+            label: 'Email',
+            thClass: 'text-center',
+            tdClass: ''
+          },
+          {
+            key: 'role',
+            sortable: true,
+            label: 'Role',
+            thClass: 'text-center',
+            tdClass: ''
+          },
+          {
+            key: 'status',
+            sortable: true,
+            label: 'Status',
+            thClass: 'text-center',
+            tdClass: ''
+          },
+          {
+            key: 'cabang',
+            sortable: true,
+            label: 'Cabang',
+            thClass: 'text-center',
+            tdClass: ''
+          },
+          {
+            key: 'created_at',
+            sortable: true,
+            label: 'Dibuat Tanggal',
+            thClass: 'text-center',
+            tdClass: ''
+          },
+          {
+            key: 'action',
+            sortable: false,
+            label: 'Action',
+            thClass: 'text-center w-10p',
+            tdClass: 'text-center'
+          },
+        ],
+        items: [],
+        loading: false,
+      },
+      paging: {
+        currentPage: 1,
+        perPage: 10
+      },
+      remove: {
+        data: {
+
+        },
+        loading: false
+      },
+      opt: {
+        perPage: [10,25,50,100],
+        role: ['admin','user','staff','accounting'],
+        cabang: ['cabang 1','cabang 2','cabang 3'],
+        status: ['aktif','non aktif']
+      }
+    }
+  },
+  mixins: [validationMixin],
+  validations: {
+    form: {
+      data: {
+        nama: {
+          required
+        },
+        email: {
+          required,
+          email
+        },
+        password: {
+          required,
+          minLength: minLength(6)
+        },
+        role: {
+          required
+        },
+        status: {
+          required
+        },
+        cabang: {
+          required
+        },
+      }
+    }
+  },
+  mounted() {
+    this.doGet()
+  },
+  methods: {
+    validateState(name) {
+      const { $dirty, $error } = this.$v.form.data[name];
+      return $dirty ? !$error : null;
+    },
+    async doGet() {
+      this.table.loading = true
+      setTimeout(() => {
+        this.table.loading = false
+        this.table.items = [
+          {
+            kode: 'Kode User',
+            nama: 'Nama User',
+            email: 'Email User',
+            role: 'Role User',
+            status: 'Status User',
+            cabang: 'Cabang User',
+            created_at: 'Tanggal Dibuat',
+          },
+        ]
+      },5000)
+    },
+    async doSave() {
+      this.$v.form.$touch();
+      if (!this.$v.form.$anyError) {
+        this.form.loading = true
+        setTimeout(() => {
+          this.form.loading = false
+          this.$bvModal.hide('modal-form')
+        }, 5000);
+      }
+    },
+    async doUpdate(item) {
+      this.form.data = {...item}
+      this.$bvModal.show('modal-form')
+    },
+    async doDelete(item,prompt) {
+      if(prompt){
+        this.remove.data = item
+        this.$bvModal.show('modal-delete')
+      }
+    }
+  }
+};
+</script>
+  
