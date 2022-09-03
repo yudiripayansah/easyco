6eac2ac8f4e347f85b29ad7db3627ddc6bcedf7b
salehibrahim <saleh.ibrahim91@gmail.com>
2022-09-03 11:51:56 +0700

    Composer Install

--- backend/vendor/composer/installed.php
+++ backend/vendor/composer/installed.php
@@ -1,11 +1,11 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => 'v8.6.12',
-        'version' => '8.6.12.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '378884bf75b2d877d62f85be1767947c0aa45365',
         'name' => 'laravel/laravel',
         'dev' => true,
     ),
@@ -395,12 +395,12 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'v8.6.12',
-            'version' => '8.6.12.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '378884bf75b2d877d62f85be1767947c0aa45365',
             'dev_requirement' => false,
         ),
         'laravel/sail' => array(
