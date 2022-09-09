50f8cfa5557b5ffb8d5bd0b3c8c60993a7309033
salehibrahim <saleh.ibrahim91@gmail.com>
2022-09-09 19:41:24 +0700

    Buat Migration Trx GL dan Trx GL Detail

---
+++ backend/app/Models/KopTrxGlDetail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KopTrxGlDetail extends Model
+{
+    use HasFactory;
+
+    protected $table = 'kop_trx_gl_detail';
+}
---
+++ backend/database/migrations/2022_09_09_122345_create_kop_trx_gl_detail_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateKopTrxGlDetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kop_trx_gl_detail', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_trx_gl_detail', 32)->default(DB::raw('uuid()'))->unique();
+            $table->string('id_trx_gl', 32);
+            $table->string('kode_gl', 20);
+            $table->char('flag_dc', 1);
+            $table->decimal('amount', 14, 2)->nullable(TRUE)->default(0);
+            $table->text('description')->nullable(TRUE);
+            $table->integer('trx_sequence')->nullable(TRUE)->default(0);
+
+            $table->foreign('id_trx_gl')->references('id_trx_gl')->on('kop_trx_gl')->cascadeOnUpdate()->restrictOnDelete();
+            $table->foreign('kode_gl')->references('kode_gl')->on('kop_gl')->cascadeOnUpdate()->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kop_trx_gl_detail');
+    }
+}
