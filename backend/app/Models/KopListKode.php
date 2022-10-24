ae2b5f2e0931721a5033d5bc4af6043aa95f39fa
salehibrahim <saleh.ibrahim91@gmail.com>
2022-10-24 11:50:31 +0700

    Buat API List Kode

--- backend/app/Models/KopListKode.php
+++ backend/app/Models/KopListKode.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Validator;
 
 class KopListKode extends Model
 {
@@ -12,4 +13,65 @@ class KopListKode extends Model
     use SoftDeletes;
 
     protected $table = 'kop_list_kode';
+    protected $fillable = ['kode_value', 'nama_kode', 'kode_display', 'created_by'];
+
+    public function validateAdd($validate)
+    {
+        $rule = [
+            'kode_value' => 'required|numeric',
+            'nama_kode' => 'required',
+            'kode_display' => 'required',
+            'created_by' => 'required'
+        ];
+
+        $validator = Validator::make($validate, $rule);
+
+        if ($validator->fails()) {
+            $errors =  $validator->errors()->all();
+
+            $res = [
+                'status' => FALSE,
+                'errors' => $errors,
+                'msg' => 'Validation Error'
+            ];
+        } else {
+            $res = [
+                'status' => TRUE,
+                'errors' => NULL,
+                'msg' => 'Validation OK'
+            ];
+        }
+
+        return $res;
+    }
+
+    public function validateUpdate($validate)
+    {
+        $rule = [
+            'id' => 'required|numeric',
+            'kode_value' => 'required|numeric',
+            'nama_kode' => 'required',
+            'kode_display' => 'required'
+        ];
+
+        $validator = Validator::make($validate, $rule);
+
+        if ($validator->fails()) {
+            $errors =  $validator->errors()->all();
+
+            $res = [
+                'status' => FALSE,
+                'errors' => $errors,
+                'msg' => 'Validation Error'
+            ];
+        } else {
+            $res = [
+                'status' => TRUE,
+                'errors' => NULL,
+                'msg' => 'Validation OK'
+            ];
+        }
+
+        return $res;
+    }
 }
