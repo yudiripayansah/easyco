e1df037df57e403f8012f1e989b0aae74921f37c
icip1998 <icip1998@gmail.com>
2023-09-22 03:29:38 +0700

    feat(laporan-keuangan-bulan-lalu): new module

---
+++ frontend/src/core/services/laporanKeuanganBulanLalu.service.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+axios.defaults.baseURL = "https://easyco.kopikoding.com/api/api/";
+
+/**
+ * BEGIN
+ * @author Irham C. <icip1998@gmail.com>
+ * @date 2023-09-22
+ */
+export default {
+    getReportSetup(payload, token) {
+        let url = "laporan/list/get_report_setup?" + payload;
+        let config = {
+            headers: {
+                token: token,
+            },
+        };
+        return axios.get(url, config);
+    }
+}
