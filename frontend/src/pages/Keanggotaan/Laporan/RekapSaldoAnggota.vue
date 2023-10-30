970505c5d951b27b05afbb1f7fe33d7e2e64978d
John Doe <johndoe@example.com>
2023-10-30 16:07:54 +0700

    update menu rekap saldo anggota menambahkan kata Rp

--- frontend/src/pages/Keanggotaan/Laporan/RekapSaldoAnggota.vue
+++ frontend/src/pages/Keanggotaan/Laporan/RekapSaldoAnggota.vue
@@ -32,6 +32,24 @@
               <template #cell(no)="item">
                 {{ item.index + 1 }}
               </template>
+              <template #cell(simwa)="table">
+              Rp. {{ table.item.simwa }}
+            </template>
+            <template #cell(simpok)="table">
+              Rp. {{ table.item.simpok }}
+            </template>
+            <template #cell(simsuk)="table">
+              Rp. {{ table.item.simsuk }}
+            </template>
+            <template #cell(saldo_pokok)="table">
+              Rp. {{ table.item.saldo_pokok }}
+            </template>
+            <template #cell(saldo_margin)="table">
+              Rp. {{ table.item.saldo_margin }}
+            </template>
+            <template #cell(saldo_catab)="table">
+              Rp. {{ table.item.saldo_catab }}
+            </template>
             </b-table>
           </b-col>
           <b-col cols="12" class="justify-content-end d-flex">
@@ -85,13 +103,13 @@
                 <tr v-for="(table, tableIndex) in table.items" :key="`table-${tableIndex}`">
                   <td class="text-center">{{ tableIndex + 1 }}</td>
                   <td class="text-left">{{ table.keterangan }}</td>
-                  <td class="text-right">{{ table.jumlah_anggota }}</td>
-                  <td class="text-right">{{ table.simwa }}</td>
-                  <td class="text-right">{{ table.simpok }}</td>
-                  <td class="text-right">{{ table.simsuk }}</td>
-                  <td class="text-right">{{ table.saldo_pokok }}</td>
-                  <td class="text-right">{{ table.saldo_margin }}</td>
-                  <td class="text-right">{{ table.saldo_catab }}</td>
+                  <td class="text-center">{{ table.jumlah_anggota }}</td>
+                  <td class="text-right">Rp. {{ table.simwa }}</td>
+                  <td class="text-right">Rp. {{ table.simpok }}</td>
+                  <td class="text-right">Rp. {{ table.simsuk }}</td>
+                  <td class="text-right">Rp. {{ table.saldo_pokok }}</td>
+                  <td class="text-right">Rp. {{ table.saldo_margin }}</td>
+                  <td class="text-right">Rp. {{ table.saldo_catab }}</td>
                 </tr>
               </tbody>
               <tbody v-else>
@@ -168,7 +186,7 @@ export default {
             sortable: true,
             label: "Jumlah Anggota",
             thClass: "text-center",
-            tdClass: "text-right",
+            tdClass: "text-center",
           },
           {
             key: "simwa",
