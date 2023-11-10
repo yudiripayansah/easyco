a0af820eadd4afc653026f1d0cf7b0d52e2506b9
icip1998 <icip1998@gmail.com>
2023-11-10 10:03:14 +0700

    feat(laporan-rekap-par): new module

---
+++ frontend/src/core/services/pembiayaanLaporanRekapPar.service.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+axios.defaults.baseURL = "https://easyco.kopikoding.com/api/api/";
+
+/**
+ * BEGIN
+ * @author Irham C. <icip1998@gmail.com>
+ * @date 2023-10-21
+ */
+export default {
+    pembiayaanLaporanRekapPar(payload, token) {
+        const url = "laporan/rekap/par";
+        const config = {
+            headers: {
+                token: token,
+            },
+        };
+        return axios.post(url, payload, config);
+    },
+    pembiayaanLaporanRekapParExportToXLSX(payload, token) {
+        const url = "laporan/rekap/excel/par?" + payload;
+        const config = {
+            headers: {
+                token: token,
+            },
+            responseType: 'blob'
+        };
+        return axios.get(url, config);
+    },
+    pembiayaanLaporanRekapParExportToCSV(payload, token) {
+        const url = "laporan/rekap/csv/par?" + payload;
+        const config = {
+            headers: {
+                token: token,
+            },
+            responseType: 'blob'
+        };
+        return axios.get(url, config);
+    }
+};
