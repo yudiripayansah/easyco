47a5f65fb8879320cc834541dc42a39d84726479
Yudi <yudi@360and5.com>
2023-11-25 15:03:41 +0700

    updates

--- frontend/src/pages/Keuangan/LaporanNeraca.vue
+++ frontend/src/pages/Keuangan/LaporanNeraca.vue
@@ -111,19 +111,19 @@ export default {
     ...helper,
     async exportPdf() {
       if (this.paging.cabang && this.paging.tanggal) {
-        const url = `https://easycop.kopikoding.com/report/print_balance_sheet_current_pdf/${this.paging.cabang}/10/${this.paging.tanggal}`;
+        const url = `https://easycop.kopsyahmsi.com/report/print_balance_sheet_current_pdf/${this.paging.cabang}/10/${this.paging.tanggal}`;
         window.open(url, "_blank");
       } else if (this.paging.cabang && this.paging.tanggal_bulan_lalu) {
-        const url = `https://easycop.kopikoding.com/report/print_balance_sheet_pdf/${this.paging.cabang}/10/${this.paging.tanggal_bulan_lalu}`;
+        const url = `https://easycop.kopsyahmsi.com/report/print_balance_sheet_pdf/${this.paging.cabang}/10/${this.paging.tanggal_bulan_lalu}`;
         window.open(url, "_blank");
       }
     },
     async exportXls() {
       if (this.paging.cabang && this.paging.tanggal) {
-        const url = `https://easycop.kopikoding.com/report/print_balance_sheet_current_xls/${this.paging.cabang}/10/${this.paging.tanggal}`;
+        const url = `https://easycop.kopsyahmsi.com/report/print_balance_sheet_current_xls/${this.paging.cabang}/10/${this.paging.tanggal}`;
         window.open(url, "_blank");
       } else if (this.paging.cabang && this.paging.tanggal_bulan_lalu) {
-        const url = `https://easycop.kopikoding.com/report/print_balance_sheet_xls/${this.paging.cabang}/10/${this.paging.tanggal_bulan_lalu}`;
+        const url = `https://easycop.kopsyahmsi.com/report/print_balance_sheet_xls/${this.paging.cabang}/10/${this.paging.tanggal_bulan_lalu}`;
         window.open(url, "_blank");
       }
     },
